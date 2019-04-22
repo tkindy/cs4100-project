000c2b4b5943dbe3ac5d2860076d3b5f48d922d9
Tyler Kindy <tylerkindy@gmail.com>
2019-04-22 12:51:48 -0400

    Clean up sarsa impl

--- Sarsa/sarsa0.py
+++ Sarsa/sarsa0.py
@@ -1,12 +1,6 @@
 import gym
-import math
-import random
-import pandas as pd
-import json
-import copy
-from random import random
 from collections import defaultdict, namedtuple
-from sympy.matrices import Matrix, ImmutableMatrix
+from sympy.matrices import ImmutableMatrix
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
@@ -14,95 +8,72 @@ import sys
 #An implementation of Sarsa specified for Centipede-ram-v0
 class Sarsa:
 
-  def __init__(self, numGames, displayPlot=True):
+  def __init__(self, numGames):
     self.env = gym.make('Centipede-ram-v0')
-    self.QTable = defaultdict(float)
-    self.FrequencyTable = defaultdict(int)
-    self.numGames = numGames 
-    self.displayPlut = displayPlot
-    
-  def getQTable(self):
-    return self.QTable
+    self.qTable = defaultdict(float)
+    self.freqTable = defaultdict(int)
+    self.numGames = numGames
 
-  def getFTable(self):
-    return self.FrequencyTable
+    self.gamma = 0.99
+    self.epsilon = 0.99
+    self.finalEpsilon = 0.01
+    self.epsilonDrop = (self.epsilon - self.finalEpsilon) / self.numGames * 2
+    self.alpha = 0.6
 
-  def getEnv(self):
-    return self.env
-
-  def getNumGames(self):
-    return self.numGames
-
-  def greedyPolicy(self, state, epsilon):
+  def greedyPolicy(self, state):
     randomValue = np.random.rand()
-    if (randomValue < epsilon):
-      return self.getEnv().action_space.sample()
+    if (randomValue < self.epsilon):
+      return self.env.action_space.sample()
 
-    qValues = {}
-    for i in range(18):
-      qValues[i] = self.getQTable()[state, i]
+    qValues = { i: self.qTable[state, i] for i in range(18) }
     maxQValue = max(qValues.values())
 
     actions_with_max_q = [a for a, q in qValues.items() if q == maxQValue]
     return np.random.choice(actions_with_max_q)
 
-  def updateQ(self, prevState, nextState, action, nextAction, reward, done, alpha, gamma):
-    qValue = self.getQTable()[nextState, nextAction]
-    self.FrequencyTable[prevState, action] = self.FrequencyTable[prevState, action] + 1
-    update = alpha * (reward + gamma * qValue * (1 - done)  - self.getQTable()[prevState, action])
-    self.getQTable()[prevState, action] += update
+  def updateQ(self, prevState, nextState, action, nextAction, reward, done):
+    qValue = self.qTable[nextState, nextAction]
+    self.freqTable[prevState, action] += 1
+    update = self.alpha * (reward + self.gamma * qValue * (1 - done)  - self.qTable[prevState, action])
+    self.qTable[prevState, action] += update
 
-  def getNewEpsilon(self, epsilon, finalEpsilon, epsilonDrop):
-    if(epsilon > finalEpsilon):
-      epsilon -= epsilonDrop
-      if(epsilon < finalEpsilon):
-        epsilon = finalEpsilon
+  def updateEpsilon(self):
+    self.epsilon = max(self.epsilon - self.epsilonDrop, self.finalEpsilon)
 
-    return epsilon
-    
   def run(self):
-    print("RUN %d Games..." % self.getNumGames())
-    utility, steps = 0, 0
-    gamma, epsilon = 0.99, 0.99
-    epsilonDecay, finalEpsilon = 0.999, 0.01
-    alpha, alphaDecay = 0.6, 0.8
+    print("RUN %d Games..." % self.numGames)
+    utility = 0
     allStates = namedtuple('allStates', ['state'])
     prevState = None
-    prevReward = 0
     prevAction = 0
-    epsilonDrop = (epsilon - finalEpsilon) / self.getNumGames() * 2
-    rewards = np.zeros(self.getNumGames())
-    for n in range(self.getNumGames()):
+    rewards = np.zeros(self.numGames)
+    for n in range(self.numGames):
       print("going through..." + str(n))
-      currState = self.getEnv().reset()
-      utility = 0 
-      currReward = 0
+      currState = self.env.reset()
+      utility = 0
       done = False
       while not done:
-        prevAction = self.greedyPolicy(allStates(ImmutableMatrix(currState)), epsilon)
-        nextState, reward, done, info = self.getEnv().step(prevAction)
+        prevAction = self.greedyPolicy(allStates(ImmutableMatrix(currState)))
+        nextState, reward, done, _ = self.env.step(prevAction)
         if reward is not None:
-          prevReward = reward
-          utility = utility + reward
+          utility += reward
 
         prevState = currState
         currState = nextState
 
-        nextAction = self.greedyPolicy(ImmutableMatrix(currState), epsilon)
+        nextAction = self.greedyPolicy(ImmutableMatrix(currState))
 
-        self.updateQ(allStates(ImmutableMatrix(prevState)), prevAction, reward, allStates(ImmutableMatrix(currState)), nextAction, done, alpha, gamma)
-        
+        self.updateQ(allStates(ImmutableMatrix(prevState)), prevAction, reward, allStates(ImmutableMatrix(currState)), nextAction, done)
 
-      epsilon = self.getNewEpsilon(epsilon, finalEpsilon, epsilonDrop) 
-      rewards[n] = utility      
+
+      self.updateEpsilon()
+      rewards[n] = utility
 
     return rewards
 
 def plotRewards(rewards):
   n = len(rewards)
-  running_avg = np.empty(n)
-  for t in range(n):
-    running_avg[t] = np.mean(rewards[max(0, t-100):(t+1)])
+  running_avg = [ np.mean(rewards[max(0, t - 100):(t + 1)]) for t in range(n) ]
   plt.plot(running_avg)
   plt.title("Reward Averages")
   plt.xlabel('iteration', fontsize=18)
@@ -113,4 +84,3 @@ numGames = int(sys.argv[1])
 s = Sarsa(numGames)
 r = s.run()
 plotRewards(r)
-
